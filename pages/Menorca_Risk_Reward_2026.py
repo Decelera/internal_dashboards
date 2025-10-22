363b12a5e6a03bed1701c3883d3c685c8f42c136
Carlos Martinez <cmartinez@sancuscapital.es>
2025-10-22 18:52:42 +0200

    ejemplo

---
+++ pages/Menorca_Risk_Reward_2026.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Menorca 2026",
+    page_icon="../.streamlit/static/favicon.png",
+    layout="wide"
+)
+
+st.markdown(body="""
+<h1 style='text-align: center;'>Decelera Menorca 2026<br>Proximamente</h1>
+""", unsafe_allow_html=True)
