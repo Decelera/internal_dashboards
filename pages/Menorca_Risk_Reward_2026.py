522325f7d267ee0981efe40d6127099865e4120b
Carlos Martinez <cmartinez@sancuscapital.es>
2025-10-22 18:54:06 +0200

    ejemplo

--- pages/Menorca_Risk_Reward_2026.py
+++ pages/Menorca_Risk_Reward_2026.py
@@ -6,6 +6,68 @@ st.set_page_config(
     layout="wide"
 )
 
+
+with st.sidebar:
+    # Home button at the top
+    if st.button("🏠 Home", key="home_btn", use_container_width=True):
+        st.switch_page("Home.py")
+    
+    st.markdown("---")
+    
+    # Mexico (Title 1)
+    st.markdown("#### Mexico")
+    
+    # Year selection in sidebar
+    st.markdown(f"&nbsp;&nbsp;&nbsp;&nbsp;**{st.session_state.selected_year}**")
+    
+    # Investment section (Title 4)
+    st.markdown("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**Investment**")
+    
+    # Investment pages (Title 5)
+    if st.button("Risk-Reward", key="mx_inv_general", use_container_width=True):
+        st.switch_page(f"pages/Mexico_Risk_Reward_{st.session_state.selected_year}.py")
+    
+    if st.button("Feedback details", key="mx_inv_startup", use_container_width=True):
+        st.switch_page(f"pages/Mexico_Feedback_Details_{st.session_state.selected_year}.py")
+    
+    # Program section (Title 4)
+    st.markdown("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**Program**")
+    
+    # Program pages (Title 5)
+    if st.button("Guests feedback", key="mx_prog_general", use_container_width=True):
+        st.switch_page(f"pages/Mexico_Guests_Feedback_{st.session_state.selected_year}.py")
+    
+    if st.button("Breathe-Focus-Grow", key="mx_prog_agenda", use_container_width=True):
+        st.switch_page(f"pages/Mexico_Breathe-Focus-Grow_{st.session_state.selected_year}.py")
+    
+    st.markdown("---")
+    
+    # Menorca (Title 1)
+    st.markdown("#### Menorca")
+    
+    # Year selection in sidebar
+    st.markdown(f"&nbsp;&nbsp;&nbsp;&nbsp;**{st.session_state.selected_year}**")
+    
+    # Investment section (Title 4)
+    st.markdown("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**Investment**")
+    
+    # Investment pages (Title 5)
+    if st.button("Risk-Reward", key="mn_inv_general", use_container_width=True):
+        st.switch_page(f"pages/Menorca_Risk_Reward_{st.session_state.selected_year}.py")
+    
+    if st.button("Feedback details", key="mn_inv_startup", use_container_width=True):
+        st.switch_page(f"pages/Menorca_Feedback_Details_{st.session_state.selected_year}.py")
+    
+    # Program section (Title 4)
+    st.markdown("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;**Program**")
+    
+    # Program pages (Title 5)
+    if st.button("Guests feedback", key="mn_prog_general", use_container_width=True):
+        st.switch_page(f"pages/Menorca_Guests_Feedback_{st.session_state.selected_year}.py")
+    
+    if st.button("Breathe-Focus-Grow", key="mn_prog_agenda", use_container_width=True):
+        st.switch_page(f"pages/Menorca_Breathe-Focus-Grow_{st.session_state.selected_year}.py")
+
 st.markdown(body="""
 <h1 style='text-align: center;'>Decelera Menorca 2026<br>Proximamente</h1>
 """, unsafe_allow_html=True)
