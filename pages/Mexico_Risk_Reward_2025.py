91cd2ca43b073ea6e178d17c2c9c4e81e5812c7e
Carlos Martinez <cmartinez@sancuscapital.es>
2025-10-30 18:27:47 +0100

    Robustez y escalabilidad

--- pages/Mexico_Risk_Reward_2025.py
+++ pages/Mexico_Risk_Reward_2025.py
@@ -5,6 +5,17 @@ import pandas as pd
 import streamlit as st
 import plotly.graph_objects as go
 
+
+#=============================================PROGRAM CONFIGURATION===============================================
+#=================================================================================================================
+
+PROGRAM_YEAR = "2025"
+PROGRAM_NAME = "Mexico 2025" #Hay que poner el programa en el formato "Localizacion Año"
+
+#=====================================PAGE CONFIGURATION=======================================================
+#=============================================================================================================
+
+
 # Page configuration
 st.set_page_config(
     page_title="Mexico - Investment - General",
@@ -12,8 +23,52 @@ st.set_page_config(
     layout="wide"
 )
 
+st.markdown("""
+<style>
+.outer-container {
+    display: flex;
+    justify-content: center; /* Centra horizontalmente */
+    width: 100%; /* Ocupa todo el ancho disponible */
+}
+.container {
+    display: flex;
+    align-items: center;
+}
+.logo-img {
+    width: 80px;
+    height: 80px;
+    margin-right: 20px;
+}
+.title-text {
+    font-size: 2.5em; /* Tamaño del título */
+    font-weight: bold;
+}
+</style>
+<div class="outer-container">
+<div class="container">
+    <img class="logo-img" src="https://images.squarespace-cdn.com/content/v1/67811e8fe702fd5553c65249/c5500619-9712-4b9b-83ee-a697212735ae/Disen%CC%83o+sin+ti%CC%81tulo+%2840%29.png">
+    <h1 class="title-text">Program Feedback<br>México 2025</h1>
+</div>
+</div>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+<style>
+h5 {
+text-align: center;
+}
+</style>
+
+<h5>Risk - Reward Matrix</h5>
+""", unsafe_allow_html=True)
+
+
+#=====================================SIDEBAR CONFIGURATION======================================================
+#================================================================================================================
+
+
 if "selected_year" not in st.session_state:
-    st.session_state.selected_year = "2025"
+    st.session_state.selected_year = PROGRAM_YEAR
 
 # Hide default Streamlit navigation elements
 st.markdown("""
@@ -85,6 +140,26 @@ with st.sidebar:
     
     if st.button("Breathe-Focus-Grow", key="mn_prog_agenda", use_container_width=True):
         st.switch_page(f"pages/Menorca_Breathe-Focus-Grow_{st.session_state.selected_year}.py")
+
+
+#========================================================FUNCTIONS CONFIGURATION=================================================
+#===============================================================================================================================
+
+def grouped_means(df):
+
+    # Calculate the mean of risk and reward scores
+    risk_mean = df[risk_reward_fields["risk_scores"]].stack().mean()
+    reward_mean = df[risk_reward_fields["reward_scores"]].stack().mean()
+
+    return pd.Series({
+        "risk_mean": risk_mean,
+        "reward_mean": reward_mean
+    })
+
+
+#=======================================================DATOS DE AIRTABLE======================================================
+#===============================================================================================================================
+
         
 api_key = st.secrets["airtable_mexico_investment"]["api_key"]
 base_id = st.secrets["airtable_mexico_investment"]["base_id"]
@@ -97,8 +172,8 @@ table_em = api.table(base_id, table_id_em)
 table_team = api.table(base_id, table_id_team)
 
 
-records_team = table_team.all(view="Mexico 2025")
-records_em = table_em.all(view="Mexico 2025")
+records_team = table_team.all(view=PROGRAM_NAME)
+records_em = table_em.all(view=PROGRAM_NAME)
 
 data_team = [record['fields'] for record in records_team]
 data_em = [record['fields'] for record in records_em]
@@ -114,12 +189,18 @@ def fix_cell(val):
 df_team = df_team.map(fix_cell)
 df_em = df_em.map(fix_cell)
 
-#----------------------------------Voy a quitar a Sean-------
-condition: bool = (df_em["EM_Name"].str.startswith("Sean"))
-df_em = df_em[~condition]
-#--------------------------------------------------------------
+#----------------------------------Voy a quitar a Sean--------------
 
-#=============CONFIG===========================
+if "EM_Name" in df_em.columns.tolist() and not df_em.empty:
+    condition: bool = (df_em["EM_Name"].str.startswith("Sean"))
+    df_em = df_em[~condition]
+#-------------------------------------------------------------------
+
+
+#=====================================FIELDS CONFIGURATION=====================================================
+#=============================================================================================================
+
+
 startup_founders = {
     "ROOK": ["Marco Benitez", "Jonas Ducker", "Daniel Martínez"],
     "Figuro": ["Juan Camilo Gonzalez"],
@@ -205,163 +286,118 @@ fields = {
     ]
 }
 
-for subject in ["risk_scores", "reward_scores"]:
-    for field in risk_reward_fields[subject]:
-        if field not in df_em.columns:
-            df_em[field] = np.nan
 
-if "Startup" not in df_em.columns:
-    df_em["Startup"] = np.nan
+#=======================================================CALCULO DE METRICAS Y GRAFICA=========================================================
+#=============================================================================================================================================
 
-#Vamos con ellooooo
 
-st.markdown("""
-<style>
-.outer-container {
-    display: flex;
-    justify-content: center; /* Centra horizontalmente */
-    width: 100%; /* Ocupa todo el ancho disponible */
-}
-.container {
-    display: flex;
-    align-items: center;
-}
-.logo-img {
-    width: 80px;
-    height: 80px;
-    margin-right: 20px;
-}
-.title-text {
-    font-size: 2.5em; /* Tamaño del título */
-    font-weight: bold;
-}
-</style>
-<div class="outer-container">
-<div class="container">
-    <img class="logo-img" src="https://images.squarespace-cdn.com/content/v1/67811e8fe702fd5553c65249/c5500619-9712-4b9b-83ee-a697212735ae/Disen%CC%83o+sin+ti%CC%81tulo+%2840%29.png">
-    <h1 class="title-text">Program Feedback<br>México 2025</h1>
-</div>
-</div>
-""", unsafe_allow_html=True)
+# Vamos a darle la vuelta a risk 
+if "Startup" in df_em.columns.tolist() and all(elem in df_em.columns.tolist() for elem in risk_reward_fields["risk_scores"]) \
+    and all(elem in df_em.columns.tolist() for elem in risk_reward_fields["reward_scores"]) and not df_em.empty:
 
-st.markdown("""
-<style>
-h5 {
-text-align: center;
-}
-</style>
+    df_em = df_em[df_em["Startup"].isin(startup_founders.keys())]
+    df_em[risk_reward_fields["risk_scores"]] = 5 - df_em[risk_reward_fields["risk_scores"]]
+    df_em[risk_reward_fields["risk_scores"]] = df_em[risk_reward_fields["risk_scores"]].replace(0, np.nan)
+    df_em[risk_reward_fields["risk_scores"]] = df_em[risk_reward_fields["risk_scores"]].replace(0, np.nan)
 
-<h5>Risk - Reward Matrix</h5>
-""", unsafe_allow_html=True)
+    df_em_means = df_em.groupby("Startup").apply(grouped_means).reset_index() #DEFINIMOS AQUI df_em_means que es donde calcularemos las medias
+else:
+    df_em_means = pd.DataFrame()
 
-#vamos a darle la vuelta a risk 
-df_em = df_em[df_em["Startup"].isin(startup_founders.keys())]
-df_em[risk_reward_fields["risk_scores"]] = 5 - df_em[risk_reward_fields["risk_scores"]]
-df_em[risk_reward_fields["risk_scores"]] = df_em[risk_reward_fields["risk_scores"]].replace(0, np.nan)
-df_em[risk_reward_fields["reward_scores"]] = df_em[risk_reward_fields["reward_scores"]].replace(0, np.nan)
-df_em[risk_reward_fields["risk_scores"]] = df_em[risk_reward_fields["risk_scores"]].replace(0, np.nan)
+if risk_reward_fields["reward_scores"] in df_em.columns.tolist():
+    df_em[risk_reward_fields["reward_scores"]] = df_em[risk_reward_fields["reward_scores"]].replace(0, np.nan)
+
 
 #vamosa calcular un par de medias
+if all(elem in df_em_means.columns.tolist() for elem in ["Startup", "risk_mean", "reward_mean"]):
 
-def grouped_means(df):
-    risk_mean = df[risk_reward_fields["risk_scores"]].stack().mean()
-    reward_mean = df[risk_reward_fields["reward_scores"]].stack().mean()
+    df_em_means["Distance"] = np.sqrt((df_em_means["risk_mean"]) ** 2 + (4 - df_em_means["reward_mean"]) ** 2)
+    num_feedback = df_em_means.groupby(by="Startup").size()
+    df_em_means["num_feedback"] = df_em_means["Startup"].map(num_feedback)
 
-    return pd.Series({
-        "risk_mean": risk_mean,
-        "reward_mean": reward_mean
-    })
+    fig: Figure = go.Figure()
 
-df_em_means = df_em.groupby("Startup").apply(grouped_means).reset_index()
-df_em_means["Distance"] = np.sqrt((df_em_means["risk_mean"]) ** 2 + (4 - df_em_means["reward_mean"]) ** 2)
-num_feedback = df_em_means.groupby(by="Startup").size()
-df_em_means["num_feedback"] = df_em_means["Startup"].map(num_feedback)
+    fig.add_trace(go.Scatter(
+        x=df_em_means["risk_mean"],
+        y=df_em_means["reward_mean"],
+        customdata=df_em_means["num_feedback"],
+        hovertemplate="Feedback enviado: %{customdata}<br>Risk: %{x:.2f}<br>Reward: %{y:.2f}<extra></extra>",
+        mode='markers+text',
+        text=df_em_means["Startup"],
+        textposition="top center",
+        marker=dict(
+            size=10,
+            color=df_em_means["risk_mean"],
+            colorscale='RdYlGn_r',
+            showscale=False,
+            colorbar=dict(
+                title="Distance to (risk=0, reward=4)"
+            )
+        )
+    ))
 
-fig: Figure = go.Figure()
-
-fig.add_trace(go.Scatter(
-    x=df_em_means["risk_mean"],
-    y=df_em_means["reward_mean"],
-    customdata=df_em_means["num_feedback"],
-    hovertemplate="Feedback enviado: %{customdata}<br>Risk: %{x:.2f}<br>Reward: %{y:.2f}<extra></extra>",
-    mode='markers+text',
-    text=df_em_means["Startup"],
-    textposition="top center",
-    marker=dict(
-        size=10,
-        color=df_em_means["risk_mean"],
-        colorscale='RdYlGn_r',
-        showscale=False,
-        colorbar=dict(
-            title="Distance to (risk=0, reward=4)"
+    fig.update_layout(
+        xaxis_title='Risk mean',
+        yaxis_title='Reward mean',
+        font=dict(
+            family="Arial, sans-serif",
+            size=10,
+            color="black"
+        ),
+        xaxis=dict(
+            showline=True,
+            linewidth=2,
+            linecolor='black',
+            gridcolor='lightgray',
+            range=[df_em_means["risk_mean"].min() - 0.1, df_em_means["risk_mean"].max() + 0.1]
+        ),
+        yaxis=dict(
+            showline=True,
+            linewidth=2,
+            linecolor='black',
+            gridcolor='lightgray',
+            range=[df_em_means["reward_mean"].min() - 0.2, df_em_means["reward_mean"].max() + 0.2]
         )
     )
-))
 
-fig.update_layout(
-    xaxis_title='Risk mean',
-    yaxis_title='Reward mean',
-    font=dict(
-        family="Arial, sans-serif",
-        size=10,
-        color="black"
-    ),
-    xaxis=dict(
-        showline=True,
-        linewidth=2,
-        linecolor='black',
-        gridcolor='lightgray',
-        range=[df_em_means["risk_mean"].min() - 0.1, df_em_means["risk_mean"].max() + 0.1]
-    ),
-    yaxis=dict(
-        showline=True,
-        linewidth=2,
-        linecolor='black',
-        gridcolor='lightgray',
-        range=[df_em_means["reward_mean"].min() - 0.2, df_em_means["reward_mean"].max() + 0.2]
-    )
-)
+    st.plotly_chart(fig)
 
-st.plotly_chart(fig)
+    st.markdown("""
+    <h4>Top Companies According to Risk-Reward Matrix</h4>
+    """, unsafe_allow_html=True)
+    df_em_ordered = df_em_means.sort_values(by="Distance", ascending=True)
 
-st.markdown("""
-<h4>Top Companies According to Risk-Reward Matrix</h4>
-""", unsafe_allow_html=True)
+    for startup in df_em_ordered["Startup"].tolist():
 
-df_em_ordered = df_em_means.sort_values(by="Distance", ascending=True)
+        #primero calculo las medias de individual y de team para ponerlas
+        df_startup = df_team[df_team["Startup"] == startup]
+        df_individual = df_team[df_team["Founder_str"].str.replace(" ", "").str.lower().isin([s.replace(" ", "").lower() for s in startup_founders[startup]])]
+        
+        mean_startup_individual = df_individual[fields["individual"]].astype(float).mean().mean()
+        mean_startup_team = df_startup[fields["team"]].astype(float).mean().mean()
+        mean_all_individual = df_team[fields["individual"]].astype(float).mean().mean()
+        mean_all_team = df_team[fields["team"]].astype(float).mean().mean()
 
-for startup in df_em_ordered["Startup"].tolist():
+        row = df_startup.iloc[0]
+        logo_data = row.get("original logo")
+        if isinstance(logo_data, list) and len(logo_data) > 0 and 'url' in logo_data[0]:
+            logo_url = logo_data[0]['url']
+        else:
+            logo_url = ""
 
-    #primero calculo las medias de individual y de team para ponerlas
-    df_startup = df_team[df_team["Startup"] == startup]
-    df_individual = df_team[df_team["Founder_str"].str.replace(" ", "").str.lower().isin([s.replace(" ", "").lower() for s in startup_founders[startup]])]
-    
-    mean_startup_individual = df_individual[fields["individual"]].astype(float).mean().mean()
-    mean_startup_team = df_startup[fields["team"]].astype(float).mean().mean()
-    mean_all_individual = df_team[fields["individual"]].astype(float).mean().mean()
-    mean_all_team = df_team[fields["team"]].astype(float).mean().mean()
+        with st.container(border=True):
 
-    row = df_startup.iloc[0]
-    logo_data = row.get("original logo")
-    if isinstance(logo_data, list) and len(logo_data) > 0 and 'url' in logo_data[0]:
-        logo_url = logo_data[0]['url']
-    else:
-        logo_url = ""
-
-    with st.container(border=True):
-
-        st.markdown(f"""
-            <div style="display: flex; align-items: left; margin-left: 0;">
-                <img src={logo_url} width="50">
-                <h4 style="margin-left: 10px; font-weight: bold; color: #333;"><a href="https://decelera-dashboards.streamlit.app/Mexico_Feedback_Details_{st.session_state.selected_year}?startup={startup}">{startup}</a></h4>
-            </div>
-            """,
-            unsafe_allow_html=True)
-        cols = st.columns(3)
-        with cols[0]:
-            st.metric(label="Distance to (risk=0, reward=4)", value=round(df_em_ordered[df_em_ordered["Startup"] == startup]["Distance"].values[0], 2))
-        with cols[1]:
-            st.metric(label="Individual Mean", value=round(mean_startup_individual, 2), delta=round(mean_startup_individual - mean_all_individual, 2))
-        with cols[2]:
-            st.metric(label="Team Mean", value=round(mean_startup_team, 2), delta=round(mean_startup_team - mean_all_team, 2))
-
+            st.markdown(f"""
+                <div style="display: flex; align-items: left; margin-left: 0;">
+                    <img src={logo_url} width="50">
+                    <h4 style="margin-left: 10px; font-weight: bold; color: #333;"><a href="https://decelera-dashboards.streamlit.app/Mexico_Feedback_Details_{st.session_state.selected_year}?startup={startup}">{startup}</a></h4>
+                </div>
+                """,
+                unsafe_allow_html=True)
+            cols = st.columns(3)
+            with cols[0]:
+                st.metric(label="Distance to (risk=0, reward=4)", value=round(df_em_ordered[df_em_ordered["Startup"] == startup]["Distance"].values[0], 2))
+            with cols[1]:
+                st.metric(label="Individual Mean", value=round(mean_startup_individual, 2), delta=round(mean_startup_individual - mean_all_individual, 2))
+            with cols[2]:
+                st.metric(label="Team Mean", value=round(mean_startup_team, 2), delta=round(mean_startup_team - mean_all_team, 2))
